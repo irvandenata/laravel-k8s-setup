39415b2c31f8a7217b768b5065a13d9e388b3a60
Irvan Denata <38544210+irvandenata@users.noreply.github.com>
2024-02-26 08:16:06 +0700

    updat

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\DB;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Welcome to the API'
+    ]);
 });
